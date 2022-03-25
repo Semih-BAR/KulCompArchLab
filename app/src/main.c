722945a257fbc7c3155dd8109ab547bc0f2cb41d
Semih-BAR <100291058+Semih-BAR@users.noreply.github.com>
2022-03-25 14:54:43 +0100

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,84 +1,200 @@
 #include <stdint.h>
 #include <stm32l4xx.h>
 
-void delay(unsigned int n) {					// een functie om te vertragen/wachten met een gekozen waarde als lengte.  delay(s) = n / (frequentie MCU)
-    volatile unsigned int delay = n;
-    while (delay--);
+int mux = 0;
+int tick = 0;
+int temperatuur = 0;
+
+void delay(unsigned int n){
+	volatile unsigned int delay = n;
+	while (delay != 0){
+		if (tick){
+			delay--;
+			tick = 0;
+		}
+	}
+}
+
+void clear(){
+	GPIOA->ODR &= ~(GPIO_ODR_OD7 | GPIO_ODR_OD5);
+	GPIOB->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+}
+
+void seg7(int n){
+	switch (n){
+		case 0:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1);
+			break;
+		case 1:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			break;
+		case 2:
+			GPIOA->ODR |= (GPIO_ODR_OD7);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD2 | GPIO_ODR_OD15 | GPIO_ODR_OD12);
+			break;
+		case 3:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD2 | GPIO_ODR_OD12);
+			break;
+		case 4:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+		case 5:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD1 | GPIO_ODR_OD2 | GPIO_ODR_OD12);
+			break;
+		case 6:
+			GPIOA->ODR |= (GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD1 | GPIO_ODR_OD15 | GPIO_ODR_OD12 | GPIO_ODR_OD2);
+			break;
+		case 7:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0);
+			break;
+		case 8:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD15 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+		case 9:
+			GPIOA->ODR |= (GPIO_ODR_OD7 | GPIO_ODR_OD5);
+			GPIOB->ODR |= (GPIO_ODR_OD0 | GPIO_ODR_OD12 | GPIO_ODR_OD1 | GPIO_ODR_OD2);
+			break;
+	}
+}
+
+void SysTick_Handler(void){
+	tick++;
+	switch(mux & 0x3){
+	case 0:
+		clear();
+		GPIOA->ODR &= ~(GPIO_ODR_OD8);
+		GPIOA->ODR &= ~(GPIO_ODR_OD15);		// 00
+		seg7(temperatuur / 1000);
+		break;
+
+	case 1:
+		clear();
+		GPIOA->ODR |= (GPIO_ODR_OD8);
+		GPIOA->ODR &= ~(GPIO_ODR_OD15);		// 10
+		seg7((temperatuur / 100) % 10);
+		break;
+
+	case 2:
+		clear();
+		GPIOA->ODR &= ~(GPIO_ODR_OD8);
+		GPIOA->ODR |= (GPIO_ODR_OD15);		// 01
+		seg7((temperatuur % 100) / 10);
+		break;
+
+	case 3:
+		clear();
+		GPIOA->ODR |= (GPIO_ODR_OD8);
+		GPIOA->ODR |= (GPIO_ODR_OD15);		// 11
+		seg7((temperatuur % 100) % 10);
+		break;
+	}
+	mux++;
 }
 
 int main(void) {
-	// AHB2 peripheral clock enable register
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;		// enable IO port B clock -- clock activeren voor GPIO B
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;		// enable IO port C clock -- clock activeren voor GPIO C
+	// CPU Frequentie = 48 MHz
+	// Systick interrupt elke 1 ms (1kHz)  --> 48000000 Hz / 1000 Hz --> Reload = 48000
+	SysTick_Config(48000);
 
+	// Interrupt aanzetten met een prioriteit van 128
+	NVIC_SetPriority(SysTick_IRQn, 128);
+	NVIC_EnableIRQ(SysTick_IRQn);
 
-    //LED 1
-    GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;		// port mode register mask van GPIOB pin 9 laag zetten
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;		// enable IO port C clock -- clock activeren voor GPIO C
 
-    GPIOB->MODER |= GPIO_MODER_MODE9_0;			// port mode register van GPIOB pin 9 op 01 zetten -> general purpose output mode
-    GPIOB->MODER &= ~GPIO_MODER_MODE9_1;
+	// Klok aanzetten
+	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
 
-    GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;			// output type register van GPIOB pin 9 laag zetten -> output push-pull (reset state)
+	// Klok selecteren, hier gebruiken we sysclk
+	RCC->CCIPR &= ~RCC_CCIPR_ADCSEL_Msk;
+	RCC->CCIPR |= RCC_CCIPR_ADCSEL_0 | RCC_CCIPR_ADCSEL_1;
 
+	// Deep powerdown modus uitzetten
+	ADC1->CR &= ~ADC_CR_DEEPPWD;
 
-    //LED 2
-    GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk;		// analoog aan LED1
+	// ADC voltage regulator aanzetten
+	ADC1->CR |= ADC_CR_ADVREGEN;
 
-    GPIOC->MODER |= GPIO_MODER_MODE13_0;
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_1;
+	// Delay a few miliseconds, see datasheet for exact timing
+	delay(4);
 
-    GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
+	// Kalibratie starten
+	ADC1->CR |= ADC_CR_ADCAL;
+	while(ADC1->CR & ADC_CR_ADCAL);
 
+	// ADC aanzetten
+	ADC1->CR |= ADC_CR_ADEN;
 
-    //Knop A
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;		// port mode register mask van GPIOB pin 13 laag zetten
+	// kanalen instellen
+	ADC1->SMPR1 |= (ADC_SMPR1_SMP6_0 | ADC_SMPR1_SMP6_1 | ADC_SMPR1_SMP6_2);
+	ADC1->SQR1 |= (ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_2);
 
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_0;		// port mode register van GPIOB pin 13 op 00 zetten -> input mode
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_1;
+	//NTC
+    GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;		// port mode register mask van GPIOA pin 0 laag zetten
 
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;		// port pull-up/pull-down register mask van GPIOB pin 13 laag zetten
+    GPIOA->MODER |= GPIO_MODER_MODE0_0;		// port mode register van GPIOA pin 0 op 11 zetten -> analog mode
+    GPIOA->MODER |= GPIO_MODER_MODE0_1;
 
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;		// port pull-up/pull-down register van GPIOB pin 13 op 01 zetten -> pull-up
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_1;
+    //7seg leds
+	GPIOA->MODER &= ~GPIO_MODER_MODE7_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE7_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;
 
+	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE5_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
 
-    //Knop B
-    GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;		// analoog aan knop A
+	GPIOB->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE0_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT0;
 
-    GPIOB->MODER &= ~GPIO_MODER_MODE14_0;
-	GPIOB->MODER &= ~GPIO_MODER_MODE14_1;
+	GPIOB->MODER &= ~GPIO_MODER_MODE12_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE12_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT12;
 
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;
+	GPIOB->MODER &= ~GPIO_MODER_MODE15_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE15_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT15;
 
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_1;
+	GPIOB->MODER &= ~GPIO_MODER_MODE1_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE1_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT1;
+
+	GPIOB->MODER &= ~GPIO_MODER_MODE2_Msk;
+	GPIOB->MODER |= GPIO_MODER_MODE2_0;
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT2;
+
+	//mux
+	GPIOA->MODER &= ~GPIO_MODER_MODE8_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE8_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT8;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE15_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE15_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT15;
+
+	GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
+	GPIOA->MODER |= GPIO_MODER_MODE6_0;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT6;
+
 
 
     while (1) {
-    	GPIOB->ODR &= ~GPIO_ODR_OD9;			// port output data register van GPIOC laag zetten -> LED 1 uitschakelen
-    	GPIOC->ODR &= ~GPIO_ODR_OD13;			// LED 2 uitschakelen
+    	// Start de ADC en wacht tot de sequentie klaar is
+    	ADC1->CR |= ADC_CR_ADSTART;
+    	while(!(ADC1->ISR & ADC_ISR_EOS));
 
-        if (!(GPIOB->IDR & GPIO_IDR_ID13) & !(GPIOB->IDR & GPIO_IDR_ID14)) {
-        	GPIOB->ODR &= ~GPIO_ODR_OD9;
-        	GPIOC->ODR &= ~GPIO_ODR_OD13;		// beide LEDs uitschakelen als beide knoppen tegelijk worden ingedrukt
-        }
-
-        else if (!(GPIOB->IDR & GPIO_IDR_ID14)) {
-        	GPIOB->ODR |= GPIO_ODR_OD9;
-			GPIOC->ODR |= GPIO_ODR_OD13;
-			delay(750000);
-			GPIOB->ODR &= ~GPIO_ODR_OD9;
-			GPIOC->ODR &= ~GPIO_ODR_OD13;
-			delay(750000);						// LEDs traag knipperen als knop B wordt ingedrukt
-		}
-
-        else if (!(GPIOB->IDR & GPIO_IDR_ID13)) {
-        	GPIOB->ODR |= GPIO_ODR_OD9;
-            GPIOC->ODR |= GPIO_ODR_OD13;
-            delay(2500000);
-            GPIOB->ODR &= ~GPIO_ODR_OD9;
-            GPIOC->ODR &= ~GPIO_ODR_OD13;
-            delay(2500000);
-            }									// LEDs snel knipperen als knop A wordt ingedrukt
+    	// Lees de waarde in
+    	temperatuur = ADC1->DR;
+    	delay(250);
     }
 }
