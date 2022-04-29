ca87570799666b22af00d99a2d373412d0c4a40c
Semih-BAR <100291058+Semih-BAR@users.noreply.github.com>
2022-04-29 11:13:42 +0200

    a
    a

--- app/src/main.c
+++ app/src/main.c
@@ -140,118 +140,17 @@ int main(void) {
 	ADC1->SMPR1 |= (ADC_SMPR1_SMP6_0 | ADC_SMPR1_SMP6_1 | ADC_SMPR1_SMP6_2);
 	//ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2 | ADC_SQR1_SQ1_3);
 
-
-
 	//NTC
     GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;		// port mode register mask van GPIOA pin 0 laag zetten
 
     GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;		// port mode register van GPIOA pin 0 op 11 zetten -> analog mode
 
-    // TIMER
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-    RCC->APB2ENR |= RCC_APB2ENR_TIM16EN;
-
-    GPIOB->MODER &= ~GPIO_MODER_MODE8_Msk;
-    GPIOB->MODER |=  GPIO_MODER_MODE8_1;
-    GPIOB->OTYPER &= ~GPIO_OTYPER_OT8;
-    GPIOB->AFR[1] = (GPIOB->AFR[1] & (~GPIO_AFRH_AFSEL8_Msk)) | (0xE << GPIO_AFRH_AFSEL8_Pos);
-
-    TIM16->PSC = 0;
-    TIM16->ARR = 24000;
-    TIM16->CCR1 = 12000;
-
-    TIM16->CCMR1 &= ~TIM_CCMR1_CC1S;
-    TIM16->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1FE;
-    TIM16->CCER |= TIM_CCER_CC1E;
-    TIM16->CCER &= ~TIM_CCER_CC1P;
-    TIM16->BDTR |= TIM_BDTR_MOE;
-    TIM16->CR1 |= TIM_CR1_CEN;
-
-    //7seg leds
-	GPIOA->MODER &= ~GPIO_MODER_MODE7_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE7_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE5_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE0_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE0_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT0;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE12_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE12_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT12;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE15_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE15_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT15;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE1_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE1_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT1;
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE2_Msk;
-	GPIOB->MODER |= GPIO_MODER_MODE2_0;
-	GPIOB->OTYPER &= ~GPIO_OTYPER_OT2;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE6_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT6;
-
-	//mux
-	GPIOA->MODER &= ~GPIO_MODER_MODE8_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE8_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT8;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE15_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE15_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT15;
-
-	GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
-	GPIOA->MODER |= GPIO_MODER_MODE6_0;
-	GPIOA->OTYPER &= ~GPIO_OTYPER_OT6;
-
-    //Knop A
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_Msk;		// port mode register mask van GPIOB pin 13 laag zetten
-
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_0;		// port mode register van GPIOB pin 13 op 00 zetten -> input mode
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_1;
-
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_Msk;		// port pull-up/pull-down register mask van GPIOB pin 13 laag zetten
-
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD13_0;		// port pull-up/pull-down register van GPIOB pin 13 op 01 zetten -> pull-up
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD13_1;
-
-	//Knop B
-	GPIOB->MODER &= ~GPIO_MODER_MODE14_Msk;		// port mode register mask van GPIOB pin 13 laag zetten
-
-	GPIOB->MODER &= ~GPIO_MODER_MODE14_0;		// port mode register van GPIOB pin 13 op 00 zetten -> input mode
-	GPIOB->MODER &= ~GPIO_MODER_MODE14_1;
-
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_Msk;		// port pull-up/pull-down register mask van GPIOB pin 13 laag zetten
-
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD14_0;		// port pull-up/pull-down register van GPIOB pin 13 op 01 zetten -> pull-up
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD14_1;
-
-    //LED 1
-    GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;		// port mode register mask van GPIOB pin 9 laag zetten
-
-    GPIOB->MODER |= GPIO_MODER_MODE9_0;			// port mode register van GPIOB pin 9 op 01 zetten -> general purpose output mode
-    GPIOB->MODER &= ~GPIO_MODER_MODE9_1;
-
-    GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;			// output type register van GPIOB pin 9 laag zetten -> output push-pull (reset state)
-
-    //LED 2
-    GPIOC->MODER &= ~GPIO_MODER_MODE13_Msk;		// analoog aan LED1
-
-    GPIOC->MODER |= GPIO_MODER_MODE13_0;
-    GPIOB->MODER &= ~GPIO_MODER_MODE13_1;
-
-    GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
-
     while (1) {
-    	printf(readNTC);
+    	value = readNTC();
+    	V = (value*3.0f)/4096.0f;
+    	R = (10000.0f*V)/(3.0f-V);
+    	temperatuur = 10*((1.0f/((logf(R/10000.0f)/3936.0f)+(1.0f/298.15f)))-273.15f);
+    	printf(temperatuur, "\n");
+    	delay(50);
     }
 }
