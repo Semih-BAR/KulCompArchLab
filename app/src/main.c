8a938d6b22c5176f9659ad72ff08d28f24aca820
Semih-BAR <100291058+Semih-BAR@users.noreply.github.com>
2022-05-13 11:34:19 +0200

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -4,10 +4,6 @@
 
 int mux = 0;
 int tick = 0;
-float temperatuur = 0.0f;
-float value;
-float V;
-float R;
 
 void delay(unsigned int n){
 	volatile unsigned int delay = n;
@@ -19,51 +15,17 @@ void delay(unsigned int n){
 	}
 }
 
-int readNTC(){
-	ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2 | ADC_SQR1_SQ1_3);
-	ADC1->SQR1 |= (ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_2);
-
-	// Start de ADC en wacht tot de sequentie klaar is
-	ADC1->CR |= ADC_CR_ADSTART;
-	while(!(ADC1->ISR & ADC_ISR_EOC));
-
-	return ADC1->DR;
-}
-
-int readPOT(){
-	ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2 | ADC_SQR1_SQ1_3);
-	ADC1->SQR1 |= (ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2);
-
-	// Start de ADC en wacht tot de sequentie klaar is
-	ADC1->CR |= ADC_CR_ADSTART;
-	while(!(ADC1->ISR & ADC_ISR_EOC));
-
-	return ADC1->DR;
-}
-
 void SysTick_Handler(void){
 	tick++;
 	mux++;
 }
 
-int __io_putchar(int ch){
+/*int __io_putchar(int ch){
     while(!(USART1->ISR & USART_ISR_TXE));
     USART1->TDR = ch;
-}
+}*/
 
 int main(void) {
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;		// enable IO port C clock -- clock activeren voor GPIO C
-
-    GPIOA->MODER &= ~GPIO_MODER_MODE9_Msk;
-    GPIOA->MODER |=  GPIO_MODER_MODE9_1;
-    GPIOA->OTYPER &= ~GPIO_OTYPER_OT9;
-    GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL9_Msk)) | (0x7 << GPIO_AFRH_AFSEL9_Pos);
-
-    GPIOA->MODER &= ~GPIO_MODER_MODE10_Msk;
-    GPIOA->AFR[1] = (GPIOA->AFR[1] & (~GPIO_AFRH_AFSEL10_Msk)) | (0x7 << GPIO_AFRH_AFSEL10_Pos);
-
     USART1->CR1 = 0;
     USART1->CR2 = 0;
     USART1->CR3 = 0;
@@ -108,23 +70,29 @@ int main(void) {
 	// kanalen instellen
 	ADC1->SMPR1 |= (ADC_SMPR1_SMP5_0 | ADC_SMPR1_SMP5_1 | ADC_SMPR1_SMP5_2);
 	ADC1->SMPR1 |= (ADC_SMPR1_SMP6_0 | ADC_SMPR1_SMP6_1 | ADC_SMPR1_SMP6_2);
-	//ADC1->SQR1 &= ~(ADC_SQR1_SQ1_0 | ADC_SQR1_SQ1_1 | ADC_SQR1_SQ1_2 | ADC_SQR1_SQ1_3);
-
-	//NTC
-    GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;		// port mode register mask van GPIOA pin 0 laag zetten
-
-    GPIOA->MODER |= GPIO_MODER_MODE0_0 | GPIO_MODER_MODE0_1;		// port mode register van GPIOA pin 0 op 11 zetten -> analog mode
 
 
+    // I2C
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+    RCC->APB1ENR1 |= RCC_APB1ENR1_I2C1EN;
+
+    GPIOB->MODER &= ~GPIO_MODER_MODE6_Msk;
+    GPIOB->MODER |=  GPIO_MODER_MODE6_1;
+    GPIOB->OTYPER |= GPIO_OTYPER_OT6;
+    GPIOB->AFR[0] = (GPIOB->AFR[0] & (~GPIO_AFRL_AFSEL6_Msk)) | (0x4 << GPIO_AFRL_AFSEL6_Pos);
+
+    GPIOB->MODER &= ~GPIO_MODER_MODE7_Msk;
+    GPIOB->MODER |=  GPIO_MODER_MODE7_1;
+    GPIOB->OTYPER |= GPIO_OTYPER_OT7;
+    GPIOB->AFR[0] = (GPIOB->AFR[0] & (~GPIO_AFRL_AFSEL7_Msk)) | (0x4 << GPIO_AFRL_AFSEL7_Pos);
+
+    I2C1->TIMINGR = 0x20303E5D;
+    I2C1->CR2 |= (I2C_CR2_AUTOEND | I2C_CR2_NACK);
+    I2C1->CR1 |= I2C_CR1_PE;
 
     while (1) {
-    	value = readNTC();
-    	V = (value*3.0f)/4096.0f;
-    	R = (10000.0f*V)/(3.0f-V);
-    	temperatuur = ((1.0f/((logf(R/10000.0f)/3936.0f)+(1.0f/298.15f)))-273.15f);
 
-    	printf("%2.2f",temperatuur);
-    	printf("°C\n\r");
+    	printf("a");
 
     	delay(1000);
     }
